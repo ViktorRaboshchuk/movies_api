d6d48dc659acb3011f39e7be1a3a3d58d072a9f3
Viktor <viktor.raboshchuk@gmail.com>
2021-03-11 15:53:04 +0300

    change APIView to generics classes

--- django_movies/movies/urls.py
+++ django_movies/movies/urls.py
@@ -8,7 +8,7 @@ from movies import views
 
 urlpatterns = [
     path('movies/', views.MovieListView.as_view()),
-    path('movie/<int:pk>/', views.MovieDetailView.as_view()),
+    path('movies/<int:pk>/', views.MovieDetailView.as_view()),
     path('review/', views.ReviewCreateView.as_view()),
     path('rating/', views.AddStarRatingView.as_view()),
     path('actors/', views.ActorsListView.as_view()),
